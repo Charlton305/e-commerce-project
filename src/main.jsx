7264e580e6fb307beac46b1b2f09f638f5c668e1
Lee Charlton <Leejcharlton@hotmail.com>
2024-01-29 09:48:46 +0000

    add favicon

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import ShopContextProvider from "./context/shop-context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
